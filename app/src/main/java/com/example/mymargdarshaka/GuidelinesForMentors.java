1ea17726a8b925b84475a792e1cd3ea5b908c73a
Kranthi <skranthi4444@gmail.com>
2021-08-24 17:12:57 +0530

    add guidelines for mentors, students layouts

---
+++ app/src/main/java/com/example/mymargdarshaka/GuidelinesForMentors.java
@@ -0,0 +1,14 @@
+package com.example.mymargdarshaka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuidelinesForMentors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guidelines_for_mentors);
+    }
+}
---
+++ app/src/main/java/com/example/mymargdarshaka/GuidelinesForStudents.java
@@ -0,0 +1,14 @@
+package com.example.mymargdarshaka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuidelinesForStudents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guidelines_for_students);
+    }
+}
