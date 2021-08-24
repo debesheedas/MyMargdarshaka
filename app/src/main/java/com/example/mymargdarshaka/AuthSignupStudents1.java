c326e4a6fa0cc28851340248f0f73d0baf3e7f48
Kranthi <skranthi4444@gmail.com>
2021-08-24 21:31:39 +0530

    add dropdown

--- app/src/main/java/com/example/mymargdarshaka/AuthSignupStudents1.java
+++ app/src/main/java/com/example/mymargdarshaka/AuthSignupStudents1.java
@@ -6,7 +6,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AuthSignupStudents1 extends AppCompatActivity {
 
@@ -21,9 +27,9 @@ public class AuthSignupStudents1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_signup_students1);
 
-        logout=(Button) findViewById(R.id.logout_student);
+        logout = (Button) findViewById(R.id.logout_student);
 
-        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,11 +38,41 @@ public class AuthSignupStudents1 extends AppCompatActivity {
                 editor.clear();
                 editor.commit();
 
-                Intent i = new Intent(AuthSignupStudents1.this,MainActivity.class);
+                Intent i = new Intent(AuthSignupStudents1.this, MainActivity.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(i);
             }
         });
+
+
+        AutoCompleteTextView ac_text_view;
+        // instantiating ac_text_view
+        ac_text_view = findViewById(R.id.ac_text_view);
+
+        // creating a list of dropdown menu options
+        ArrayList<String> classes = new ArrayList<>();
+        classes.add("Class 6");
+        classes.add("Class 7");
+        classes.add("Class 8");
+        classes.add("Class 9");
+        classes.add("Class 10");
+        classes.add("Class 11");
+        classes.add("Class 12");
+
+        // creating a classAdapter, I'm not sure what this does but it works
+        ArrayAdapter<String> classesAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,    // this is the style of each option,
+                                                        // but I just used something simple
+                classes
+        );
+
+        // attaching the adapter to ac_text_view
+        ac_text_view.setAdapter(classesAdapter);
+
+        // adding a listener which responds to an option selection
+        // using a toast to demonstrate
+        ac_text_view.setOnItemClickListener((parent, view, position, id) -> Toast.makeText(getApplicationContext(), "Selected Item: " + classes.get(position), Toast.LENGTH_SHORT).show());
 
     }
 }
--- app/src/main/java/com/example/mymargdarshaka/Test.java
+++ app/src/main/java/com/example/mymargdarshaka/Test.java
@@ -5,10 +5,17 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.card.MaterialCardView;
+
 public class Test extends AppCompatActivity {
+    private MaterialCardView card_question;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        card_question = findViewById(R.id.card_question);
     }
+
+
 }
