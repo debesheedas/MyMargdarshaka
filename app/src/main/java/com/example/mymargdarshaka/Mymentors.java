a8ca09ba39b6b80bba0075d1dc57d77f498ca5a5
shree675 <shreeteshm37@gmail.com>
2021-08-24 16:22:14 +0530

    added user schema

--- app/src/main/java/com/example/mymargdarshaka/Mymentors.java
+++ app/src/main/java/com/example/mymargdarshaka/Mymentors.java
@@ -5,14 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class Mymentors extends AppCompatActivity {
 
     Button logout;
 
     SharedPreferences sharedPreferences;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     private static final String SHARED_PREF_NAME = "login";
 
@@ -28,14 +38,35 @@ public class Mymentors extends AppCompatActivity {
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.clear();
-                editor.commit();
+//                SharedPreferences.Editor editor = sharedPreferences.edit();
+//                editor.clear();
+//                editor.commit();
+//
+//                Intent i = new Intent(Mymentors.this,MainActivity.class);
+//                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//                startActivity(i);
 
-                Intent i = new Intent(Mymentors.this,MainActivity.class);
-                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(i);
+                firebaseDatabase=FirebaseDatabase.getInstance();
+                databaseReference=firebaseDatabase.getReference("users");
+                String key=databaseReference.push().getKey();
+                databaseReference=databaseReference.child(key);
+
+                ArrayList<String> intr=new ArrayList<>();
+                intr.add("Maths");
+                intr.add("Science");
+
+                ArrayList<String> times=new ArrayList<>();
+                times.add("10:30");
+
+                ArrayList<Pair<String,String>> reg=new ArrayList<>();
+                reg.add(new Pair<String,String>("Maths","mentorid1"));
+                reg.add(new Pair<String,String>("Science","mentorid2"));
+
+                UserDetails userSchema = new UserDetails("Somename","abc@gmail.com","9898989888","6","Hindi",intr,times,reg);
+                databaseReference.setValue(userSchema);
+
             }
         });
+
     }
 }
---
+++ app/src/main/java/com/example/mymargdarshaka/UserSchema.java
@@ -0,0 +1,25 @@
+package com.example.mymargdarshaka;
+
+import android.util.Pair;
+
+import java.util.ArrayList;
+
+public class UserSchema {
+    String id;
+    UserDetails userDetails;
+
+    public UserSchema(){}
+
+    public UserSchema(String id, String name, String email, String phone, String standard, String prefLang, ArrayList<String> intrSubjects, ArrayList<String> timeSlots, ArrayList<Pair<String,String>> regSubjects){
+        this.id=id;
+        this.userDetails=new UserDetails(name,email,phone,standard,prefLang,intrSubjects,timeSlots,regSubjects);
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public UserDetails getDetails(){
+        return this.userDetails;
+    }
+}
