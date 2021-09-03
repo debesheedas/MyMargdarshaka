6a0eac60f2c62ff0b19269d143ae4a25ca7d34b8
AASHRITH1903 <cs19b025@iittp.ac.in>
2021-09-03 14:00:59 +0530

    put all the checkboxes in a loop

--- app/src/main/java/com/example/mymargdarshaka/AuthSignupMentors2.java
+++ app/src/main/java/com/example/mymargdarshaka/AuthSignupMentors2.java
@@ -10,11 +10,13 @@ import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.common.io.LineReader;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -55,130 +57,7 @@ public class AuthSignupMentors2 extends AppCompatActivity {
         //use the arrayLIst allCheckBoxes to check if atleast 1 checkbox is checked, inside onListener for Submit button
         //display toast if not checked
 
-//        Button submit_button = findViewById(R.id.mentorsSignupButton2);
-//        submit_button.setOnClickListener(view -> {
-//
-//            // helper variable for determining which classes a mentor can teach
-//            int temp = 0;
-//
-//            if(((CheckBox)findViewById(R.id.check_english6)).isChecked()) teachSubjects.add("english6");
-//            if(((CheckBox)findViewById(R.id.check_hindi6)).isChecked()) teachSubjects.add("hindi6");
-//            if(((CheckBox)findViewById(R.id.check_telugu6)).isChecked()) teachSubjects.add("telugu6");
-//            if(((CheckBox)findViewById(R.id.check_math6)).isChecked()) teachSubjects.add("math6");
-//            if(((CheckBox)findViewById(R.id.check_science6)).isChecked()) teachSubjects.add("science6");
-//            if(((CheckBox)findViewById(R.id.check_social6)).isChecked()) teachSubjects.add("social6");
-//
-//            // if the mentor can teach atleast one subject in a class, the mentor is said to teach that class.
-//            if(temp < teachSubjects.size()){
-//                classes.add("6");
-//                temp = teachSubjects.size();
-//            }
-//
-//            if(((CheckBox)findViewById(R.id.check_english7)).isChecked()) teachSubjects.add("english7");
-//            if(((CheckBox)findViewById(R.id.check_hindi7)).isChecked()) teachSubjects.add("hindi7");
-//            if(((CheckBox)findViewById(R.id.check_telugu7)).isChecked()) teachSubjects.add("telugu7");
-//            if(((CheckBox)findViewById(R.id.check_math7)).isChecked()) teachSubjects.add("math7");
-//            if(((CheckBox)findViewById(R.id.check_science7)).isChecked()) teachSubjects.add("science7");
-//            if(((CheckBox)findViewById(R.id.check_social7)).isChecked()) teachSubjects.add("social7");
-//
-//            if(temp < teachSubjects.size()){
-//                classes.add("7");
-//                temp = teachSubjects.size();
-//            }
-//
-//            if(((CheckBox)findViewById(R.id.check_english8)).isChecked()) teachSubjects.add("english8");
-//            if(((CheckBox)findViewById(R.id.check_hindi8)).isChecked()) teachSubjects.add("hindi8");
-//            if(((CheckBox)findViewById(R.id.check_telugu8)).isChecked()) teachSubjects.add("telugu8");
-//            if(((CheckBox)findViewById(R.id.check_math8)).isChecked()) teachSubjects.add("math8");
-//            if(((CheckBox)findViewById(R.id.check_science8)).isChecked()) teachSubjects.add("science8");
-//            if(((CheckBox)findViewById(R.id.check_social8)).isChecked()) teachSubjects.add("social8");
-//
-//            if(temp < teachSubjects.size()){
-//                classes.add("8");
-//                temp = teachSubjects.size();
-//            }
-//
-//            if(((CheckBox)findViewById(R.id.check_english9)).isChecked()) teachSubjects.add("english9");
-//            if(((CheckBox)findViewById(R.id.check_hindi9)).isChecked()) teachSubjects.add("hindi9");
-//            if(((CheckBox)findViewById(R.id.check_telugu9)).isChecked()) teachSubjects.add("telugu9");
-//            if(((CheckBox)findViewById(R.id.check_math9)).isChecked()) teachSubjects.add("math9");
-//            if(((CheckBox)findViewById(R.id.check_science9)).isChecked()) teachSubjects.add("science9");
-//            if(((CheckBox)findViewById(R.id.check_social9)).isChecked()) teachSubjects.add("social9");
-//
-//            if(temp < teachSubjects.size()){
-//                classes.add("9");
-//                temp = teachSubjects.size();
-//            }
-//
-//            if(((CheckBox)findViewById(R.id.check_english10)).isChecked()) teachSubjects.add("english10");
-//            if(((CheckBox)findViewById(R.id.check_hindi10)).isChecked()) teachSubjects.add("hindi10");
-//            if(((CheckBox)findViewById(R.id.check_telugu10)).isChecked()) teachSubjects.add("telugu10");
-//            if(((CheckBox)findViewById(R.id.check_math10)).isChecked()) teachSubjects.add("math10");
-//            if(((CheckBox)findViewById(R.id.check_science10)).isChecked()) teachSubjects.add("science10");
-//            if(((CheckBox)findViewById(R.id.check_social10)).isChecked()) teachSubjects.add("social10");
-//
-//            if(temp < teachSubjects.size()){
-//                classes.add("10");
-//                temp = teachSubjects.size();
-//            }
-//
-//            if(((CheckBox)findViewById(R.id.check_english11)).isChecked()) teachSubjects.add("english11");
-//            if(((CheckBox)findViewById(R.id.check_hindi11)).isChecked()) teachSubjects.add("hindi11");
-//            if(((CheckBox)findViewById(R.id.check_telugu11)).isChecked()) teachSubjects.add("telugu11");
-//            if(((CheckBox)findViewById(R.id.check_math11)).isChecked()) teachSubjects.add("math11");
-//            if(((CheckBox)findViewById(R.id.check_physics11)).isChecked()) teachSubjects.add("physics11");
-//            if(((CheckBox)findViewById(R.id.check_chemistry11)).isChecked()) teachSubjects.add("chemistry11");
-//            if(((CheckBox)findViewById(R.id.check_history11)).isChecked()) teachSubjects.add("history11");
-//            if(((CheckBox)findViewById(R.id.check_geography11)).isChecked()) teachSubjects.add("geography11");
-//
-//            if(temp < teachSubjects.size()){
-//                classes.add("11");
-//                temp = teachSubjects.size();
-//            }
-//
-//            if(((CheckBox)findViewById(R.id.check_english12)).isChecked()) teachSubjects.add("english12");
-//            if(((CheckBox)findViewById(R.id.check_hindi12)).isChecked()) teachSubjects.add("hindi12");
-//            if(((CheckBox)findViewById(R.id.check_telugu12)).isChecked()) teachSubjects.add("telugu12");
-//            if(((CheckBox)findViewById(R.id.check_math12)).isChecked()) teachSubjects.add("math12");
-//            if(((CheckBox)findViewById(R.id.check_physics12)).isChecked()) teachSubjects.add("physics12");
-//            if(((CheckBox)findViewById(R.id.check_chemistry12)).isChecked()) teachSubjects.add("chemistry12");
-//            if(((CheckBox)findViewById(R.id.check_history12)).isChecked()) teachSubjects.add("history12");
-//            if(((CheckBox)findViewById(R.id.check_geography12)).isChecked()) teachSubjects.add("geography12");
-//
-//            if(temp < teachSubjects.size()){
-//                classes.add("12");
-//                temp = teachSubjects.size();
-//            }
-//
-//            // push mentor details to DB before taking test, and set the noTest to 1 as he is going to take a test after signup
-//            DatabaseReference newMentorRef = FirebaseDatabase.getInstance().getReference("mentors").push();
-//            String key = newMentorRef.getKey();
-//            newMentorRef.setValue(new MentorDetails(
-//                    name,
-//                    email,
-//                    phone,
-//                    classes,
-//                    prefLangs,
-//                    timeSlots,
-//                    regStudents,
-//                    teachSubjects,
-//                    1)
-//            ).addOnCompleteListener(task -> {
-//                sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
-//                SharedPreferences.Editor editor = sharedPreferences.edit();
-//                editor.putString(TYPE,"mentor");
-//                editor.putString(USER_ID,key);
-//                editor.apply();
-//
-//                // send the mentor to take the Test
-//                Intent i = new Intent(AuthSignupMentors2.this, Test.class);
-//                i.putExtra("firstTime",true);
-//                i.putExtra("mentorId",key);
-//                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-//                startActivity(i);
-//            });
-//        });
-
+        // for rendering UI elements ( checkboxes ) dynamically.
         ArrayList<String> subjects = new ArrayList<>();
         subjects.add("english");
         subjects.add("hindi");
@@ -227,6 +106,83 @@ public class AuthSignupMentors2 extends AppCompatActivity {
                 root.addView(card);
             }
         }
+
+        Button submitButton = findViewById(R.id.mentorsSignupButton2);
+        submitButton.setOnClickListener(view -> {
+
+            // helper variable for determining which classes a mentor can teach
+            int temp = 0;
+
+            for(String c : junior_class_options){
+
+                if(((CheckBox)root.findViewWithTag("check_english"+c)).isChecked()) teachSubjects.add("english"+c);
+                if(((CheckBox)root.findViewWithTag("check_hindi"+c)).isChecked()) teachSubjects.add("hindi"+c);
+                if(((CheckBox)root.findViewWithTag("check_telugu"+c)).isChecked()) teachSubjects.add("telugu"+c);
+                if(((CheckBox)root.findViewWithTag("check_math"+c)).isChecked()) teachSubjects.add("math"+c);
+                if(((CheckBox)root.findViewWithTag("check_science"+c)).isChecked()) teachSubjects.add("science"+c);
+                if(((CheckBox)root.findViewWithTag("check_social"+c)).isChecked()) teachSubjects.add("social"+c);
+
+                // if the mentor can teach atleast one subject in a class, the mentor is said to teach that class.
+                if(temp < teachSubjects.size()){
+                    classes.add(c);
+                    temp = teachSubjects.size();
+                }
+            }
+
+
+            for(String c : senior_class_options){
+
+                if(((CheckBox)root.findViewWithTag("check_english"+c)).isChecked()) teachSubjects.add("english"+c);
+                if(((CheckBox)root.findViewWithTag("check_hindi"+c)).isChecked()) teachSubjects.add("hindi"+c);
+                if(((CheckBox)root.findViewWithTag("check_telugu"+c)).isChecked()) teachSubjects.add("telugu"+c);
+                if(((CheckBox)root.findViewWithTag("check_math"+c)).isChecked()) teachSubjects.add("math"+c);
+                if(((CheckBox)root.findViewWithTag("check_physics"+c)).isChecked()) teachSubjects.add("physics"+c);
+                if(((CheckBox)root.findViewWithTag("check_chemistry"+c)).isChecked()) teachSubjects.add("chemistry"+c);
+                if(((CheckBox)root.findViewWithTag("check_biology"+c)).isChecked()) teachSubjects.add("biology"+c);
+                if(((CheckBox)root.findViewWithTag("check_history"+c)).isChecked()) teachSubjects.add("history"+c);
+                if(((CheckBox)root.findViewWithTag("check_geography"+c)).isChecked()) teachSubjects.add("geography"+c);
+
+                if(temp < teachSubjects.size()){
+                    classes.add(c);
+                    temp = teachSubjects.size();
+                }
+
+            }
+
+            // if the mentor didn't select any subjects
+            if(teachSubjects.size() == 0){
+                Toast.makeText(AuthSignupMentors2.this, "Please select atleast one subject ", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // push mentor details to DB before taking the test, and set the noTest to 1 as he is going to take a test after signup
+            DatabaseReference newMentorRef = FirebaseDatabase.getInstance().getReference("mentors").push();
+            newMentorRef.setValue(new MentorDetails(
+                    name,
+                    email,
+                    phone,
+                    classes,
+                    prefLangs,
+                    timeSlots,
+                    regStudents,
+                    teachSubjects,
+                    1)
+            ).addOnCompleteListener(task -> {
+                sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(TYPE,"mentor");
+                editor.putString(USER_ID, newMentorRef.getKey());
+                editor.apply();
+
+                // send the mentor to take the Test
+                Intent i = new Intent(AuthSignupMentors2.this, Test.class);
+                i.putExtra("firstTime",true);
+                i.putExtra("mentorId", newMentorRef.getKey());
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            });
+        });
+
     }
 
     private int dpAsPixels(int dp) {
